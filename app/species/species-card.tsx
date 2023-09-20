94a863dfcf661e4cd754d75f925bfd857b9af47a
Dinesh Vasireddy <dineshvasireddy@college.harvard.edu>
2023-09-20 06:21:17 -0400

    Update species-card.tsx

--- app/species/species-card.tsx
+++ app/species/species-card.tsx
@@ -96,16 +96,14 @@ export default function SpeciesCard(species: Species) {
     const supabase = createClientComponentClient<Database>();
     const { error } = await supabase
       .from("species")
-      .update([
-        {
-          common_name: input.common_name,
-          description: input.description,
-          kingdom: input.kingdom,
-          scientific_name: input.scientific_name,
-          total_population: input.total_population,
-          image: input.image,
-        },
-      ])
+      .update({
+        common_name: input.common_name,
+        description: input.description,
+        kingdom: input.kingdom,
+        scientific_name: input.scientific_name,
+        total_population: input.total_population,
+        image: input.image,
+      })
       .eq("scientific_name", species.scientific_name);
 
     if (error) {
